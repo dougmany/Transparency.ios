1e2db00685d57973fa4338c07acc57cba3b3acca
Doug Meeker <wog400@hotmail.com>
2020-12-19 08:26:18 -0800

    Initial Commit

---
+++ FacilitySearch3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacilitySearch3
+//
+//  Created by Doug Meeker on 12/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacilitySearch3/FacilitySearch3App.swift
@@ -0,0 +1,17 @@
+//
+//  FacilitySearch3App.swift
+//  FacilitySearch3
+//
+//  Created by Doug Meeker on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct FacilitySearch3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
